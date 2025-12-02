e1dca2547389a6a1d24f40b8b5312382234afb2b
AnndreJunior <anndre.junior15@gmail.com>
2025-12-02 20:53:34 -0300

    chore: add dotnet projects

---
+++ ProductCatalog.Api/HelloWorld/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatalog.Api.HelloWorld;
+
+[Route("[controller]")]
+[ApiController]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType<string>(StatusCodes.Status200OK)]
+    public IActionResult HelloWorld() => Ok("Hello World!");
+}
